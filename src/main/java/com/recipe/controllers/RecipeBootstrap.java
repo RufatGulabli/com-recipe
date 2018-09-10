2acf7bbf431f12125af211ce53a94bcf57de3032
Rufat Gulabli <rufatg@globaltravel.int>
2018-09-10 14:52:22 +0400

    Lombok refactoring

--- src/main/java/com/recipe/controllers/RecipeBootstrap.java
+++ src/main/java/com/recipe/controllers/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import com.recipe.domain.*;
 import com.recipe.repositories.CategoryRepository;
 import com.recipe.repositories.RecipeRepository;
 import com.recipe.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -33,6 +35,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("loading recipes ...");
     }
 
     public List<Recipe> getRecipes(){
